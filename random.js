396ea360247d923adf9b5a325ab3929646cb6c8c
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-07-10 16:27:13 -0430

    agregando el if - modidificando el código y creando la ejecución

--- random.js
+++ random.js
@@ -1,3 +1,12 @@
 // Match.random()
+// VARIABLES
+var num = Math.random();
+var str = ' MAYOR que 0,5';
 
-var num = Match.random()
+//codigo
+if (num <= 0,5) {
+      str=' MENOR que 0,5';
+}
+//fin del if
+
+console.log('\n' + num + str + '\n');
